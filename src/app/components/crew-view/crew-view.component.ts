55deea37fcbc158260daeec24fe5b866278e94af
Atif Mahmud <atifmahmud101@gmail.com>
2018-01-15 14:54:09 -0500

    Design CrewViewComponent

--- src/app/components/crew-view/crew-view.component.ts
+++ src/app/components/crew-view/crew-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-crew-view',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew-view.component.scss']
 })
 export class CrewViewComponent implements OnInit {
+  @Input() items: Array<any>;
 
   constructor() { }
 
