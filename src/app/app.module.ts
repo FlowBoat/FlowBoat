e8c51f12d4ae12503aba474841359b5db79f674b
Atif Mahmud <atifmahmud101@gmail.com>
2018-01-29 18:53:23 -0500

    Generate ServicesComponent scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { ServicesComponent } from './components/services/services.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    BannerComponent
+    BannerComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule
