973c556600e2d0baf1b41020efa055806bcb4e51
Atif Mahmud <atifmahmud101@gmail.com>
2018-01-30 11:54:41 -0500

    Create member model

---
+++ src/app/models/member.ts
@@ -0,0 +1,49 @@
+export enum SocialType {
+  facebook = 'Facebook',
+  twitter = 'Twitter',
+  youtube = 'Youtube',
+  instagram = 'Instagram',
+  website = 'Website'
+}
+
+export enum Role {
+  CEO = 'CEO',
+  projectManager = 'Project Manager',
+  leadDev = 'Lead Developer',
+  developer = 'Developer',
+  marketing = 'Marketing',
+  operations = 'Business Operations',
+  dataAnalyist = 'Data Analyst'
+}
+
+export enum School {
+  wci = 'Waterloo Collegiate Institute',
+  sjam = 'Sir John A Macdonald',
+  ccvi = 'Centennial Collegiate Vocational Institute'
+}
+
+export interface Team {
+  name: string;
+  logo: string;
+  img: string;
+  link: string;
+}
+
+export interface Social {
+  type: SocialType;
+  icon: string;
+  link: string; // FontAwesome code ie. "fa fa-facebook"
+}
+
+export interface Member {
+  name: string;
+  img: string;
+  age: number;
+  school: School;
+  team: Team;
+  role: Role;
+  tagline: string;
+  desc: string;
+  socials: Array<Social>;
+  link: string;
+}
