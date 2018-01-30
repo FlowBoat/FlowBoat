1d650135f768c77382a630c6244a6707f5c63243
Atif Mahmud <atifmahmud101@gmail.com>
2018-01-30 11:46:03 -0500

    Add projects to home. Insert dummy data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Project } from './models/project';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,78 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  projects: Array<Project> = [
+    {
+      tags: [
+        true,
+        false,
+        false,
+        false
+      ],
+      img: 'assets/images/work-1.jpg',
+      title: 'Flowi',
+      desc: 'The smart advertising solution for dense city centres.',
+      link: '#'
+    },
+    {
+      tags: [
+        false,
+        true,
+        false,
+        true
+      ],
+      img: 'assets/images/work-2.jpg',
+      title: 'BloomBud',
+      desc: 'Description for bloom bud',
+      link: '#'
+    },
+    {
+      tags: [
+        false,
+        true,
+        true,
+        false
+      ],
+      img: 'assets/images/work-3.jpg',
+      title: 'SmartWater',
+      desc: 'Description for smartwater',
+      link: '#'
+    },
+    {
+      tags: [
+        false,
+        true,
+        false,
+        true
+      ],
+      img: 'assets/images/work-4.jpg',
+      title: 'Pocket weather stations',
+      desc: 'Description for weather',
+      link: '#'
+    },
+    {
+      tags: [
+        false,
+        false,
+        false,
+        true
+      ],
+      img: 'assets/images/work-5.jpg',
+      title: 'Malarkey',
+      desc: 'Description for Malarkey',
+      link: '#'
+    },
+    {
+      tags: [
+        false,
+        false,
+        false,
+        false
+      ],
+      img: 'assets/images/work-6.jpg',
+      title: 'More projects on the way!',
+      desc: 'Description filler',
+      link: '#'
+    }
+  ];
 }
