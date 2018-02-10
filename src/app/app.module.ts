850a56ba7870da0c7506689b6aca267c6d8aab37
Atif Mahmud <atifmahmud101@gmail.com>
2018-02-10 15:16:23 -0500

    Generate about scaffold

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ServicesComponent } from './components/services/services.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { TeamComponent } from './components/team/team.component';
+import { QuotesComponent } from './components/quotes/quotes.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    ServicesComponent,
+    PresentationComponent,
+    ProjectsComponent,
+    TeamComponent,
+    QuotesComponent,
+    BlogComponent,
+    FooterComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
