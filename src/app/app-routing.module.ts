2b226b42002432483608d3b8f9af14ed17a7e9b4
Atif Mahmud <atifmahmud101@gmail.com>
2018-02-11 12:43:45 -0500

    Add routing to page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { FlowiPageComponent } from './pages/flowi-page/flowi-page.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'Flowi', pathMatch: 'full', component: FlowiPageComponent },
   { path: '404', component: PageNotFoundComponent },
   { path: '**', redirectTo: '/404'}
 ];
